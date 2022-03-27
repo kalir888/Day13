98c7fe6da56465a47c305e6c22ca02bb6ffd5faf
Kalir888 <kalir888@gmail.com>
2022-03-27 21:24:33 +0530

    Tested: Maximum of three Strings found on positon 2

--- Day13Assignment/src/com/company/TestMaximum.java
+++ Day13Assignment/src/com/company/TestMaximum.java
@@ -7,8 +7,8 @@ public class TestMaximum {
 
     public static void main(String[] args) {
         List<String> strings = new ArrayList<>();
-        strings.add("Ramachandran");
         strings.add("Geetha");
+        strings.add("Ramachandran");
         strings.add("Kalimuthu");
         strings.sort(String::compareTo);
         System.out.println("The Maximum of three Strings is " + strings.get(2));
