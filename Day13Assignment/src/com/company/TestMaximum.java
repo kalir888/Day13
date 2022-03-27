7b877e87d0a94d0bee551f6a24993124650a4221
Kalir888 <kalir888@gmail.com>
2022-03-27 21:16:02 +0530

    Tested: Maximum of three floats found on positon 2

--- Day13Assignment/src/com/company/TestMaximum.java
+++ Day13Assignment/src/com/company/TestMaximum.java
@@ -9,7 +9,7 @@ public class TestMaximum {
     public static void main(String[] args) {
         List<Float> integers = new ArrayList<>();
         integers.add(100.5f);
-        integers.add(78.5f);
+        integers.add(102.5f);
         integers.add(50.5f);
         integers.sort(Float::compareTo);
         System.out.println("The Maximum of three numbers is " + integers.get(2));
