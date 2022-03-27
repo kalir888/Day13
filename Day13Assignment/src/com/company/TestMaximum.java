2e3acd83278269c0a8713e6069ef008b34a00236
Kalir888 <kalir888@gmail.com>
2022-03-27 21:01:26 +0530

    Tested: Maximum found on position 2

--- Day13Assignment/src/com/company/TestMaximum.java
+++ Day13Assignment/src/com/company/TestMaximum.java
@@ -9,7 +9,7 @@ public class TestMaximum {
     public static void main(String[] args) {
         List<Integer> integers = new ArrayList<>();
         integers.add(589);
-        integers.add(300);
+        integers.add(689);
         integers.add(100);
         integers.sort(Integer::compareTo);
         System.out.println("The Maximum of three numbers is " + integers.get(2));
