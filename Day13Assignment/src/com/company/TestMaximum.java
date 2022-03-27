39339ab97c6fc82926b8a14c510c0dfdeb827aa2
Kalir888 <kalir888@gmail.com>
2022-03-27 20:49:15 +0530

    Added: Method to find Maximum of three Integers

--- Day13Assignment/src/com/company/TestMaximum.java
+++ Day13Assignment/src/com/company/TestMaximum.java
@@ -1,9 +1,18 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class TestMaximum {
 
     public static void main(String[] args) {
-        
+        List<Integer> integers = new ArrayList<>();
+        integers.add(500);
+        integers.add(300);
+        integers.add(100);
+        integers.sort(Integer::compareTo);
+        System.out.println("The Maximum of three numbers is " + integers.get(2));
         
     }
 }
