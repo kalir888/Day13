9c446cf6ec6726c406d368cb9464aff3d844de14
Kalir888 <kalir888@gmail.com>
2022-04-03 15:29:13 +0530

    refactoring

--- Day13Assignment/src/com/company/TestMaximum.java
+++ Day13Assignment/src/com/company/TestMaximum.java
@@ -6,8 +6,18 @@ import java.util.List;
 
 public class TestMaximum {
 
+    public static Float findMax(Float x, Float y, Float z) {
+        Float max = x;
+        if(x.compareTo(y) > 0)
+            max = y;
+        if(y.compareTo(z) > 0)
+            max = z;
+        return max;
+    }
+
     public static void main(String[] args) {
-        
+      Float  floatMax = findMax(465.2f,6498.5f,496498.56f);
+        System.out.println(floatMax);
         
     }
 }
