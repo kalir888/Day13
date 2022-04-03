f0bc266d13282087f6f43e46690e428c90f1d44a
Kalir888 <kalir888@gmail.com>
2022-04-03 15:07:16 +0530

    Modified: finding max for integer method

--- Day13Assignment/src/com/company/TestMaximum.java
+++ Day13Assignment/src/com/company/TestMaximum.java
@@ -6,13 +6,17 @@ import java.util.List;
 
 public class TestMaximum {
 
+    public static Integer findMax(Integer x, Integer y, Integer z) {
+        Integer max = x;
+        if(y.compareTo(max) > 0)
+            max = y;
+        if(z.compareTo(max) > 0)
+            max = z;
+        return max;
+    }
+
     public static void main(String[] args) {
-        List<Integer> integers = new ArrayList<>();
-        integers.add(500);
-        integers.add(300);
-        integers.add(100);
-        integers.sort(Integer::compareTo);
-        System.out.println("The Maximum of three numbers is " + integers.get(2));
+
         
     }
 }
