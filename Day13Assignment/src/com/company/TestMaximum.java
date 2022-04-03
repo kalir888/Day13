5ea79eff45b0bb68da7e1c4198fdd41faec574d7
Kalir888 <kalir888@gmail.com>
2022-04-03 14:54:03 +0530

    just to push

--- Day13Assignment/src/com/company/TestMaximum.java
+++ Day13Assignment/src/com/company/TestMaximum.java
@@ -6,12 +6,24 @@ import java.util.List;
 public class TestMaximum {
 
     public static void main(String[] args) {
-        List<String> strings = new ArrayList<>();
-        strings.add("Geetha");
-        strings.add("Kalimuthu");
-        strings.add("Ramachandran");
-        strings.sort(String::compareTo);
-        System.out.println("The Maximum of three Strings is " + strings.get(2));
+        List integers = new ArrayList();
+        integers.add(200);
+        integers.add(500);
+        integers.add(489);
+        Class class1 = integers.get(0).getClass();
+        String name = class1.getName();
+        System.out.println(name);
+
+        CompareMethod compare = new CompareMethod();
+
+        Class class2 = compare.getClass();
+        System.out.println(class2);
+        String name2 = class2.getName();
+        System.out.println(name2);
+
+        System.out.println("The Maximum of three Strings is " + integers.get(2));
+
+
         
     }
 }
